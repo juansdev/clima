1f7d417cc61bb7f12361e141233cf2693dacf60c
juansdev <juansdev72@gmail.com>
2023-03-11 12:52:10 -0500

    FEAT: Laravel Mix con React - Eliminar archivos innecesarios del Frontend default de Laravel
    Signed-off-by: juansdev <juansdev72@gmail.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+            <p>This is a React component.</p>
+        </div>
+    );
+}
+
+createRoot(document.getElementById('root')).render(<App/>);
